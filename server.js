0059f9376f24af940d172ac24a0dc606233313dc
jasonstevens13 <jasonstevens13@yahoo.com>
2020-05-09 22:54:16 -0400

    added/defined GET routes for notes.html and index.html

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // Dependencies
-// =============================================================
 var express = require("express");
 var path = require("path");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = 9000;
 
@@ -13,7 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+// Routes:
+// notes.html route
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+// index.html route
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
 
+
+
+
+// Listener (starts server)
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
