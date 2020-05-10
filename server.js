b28807b2561f9bab87ae58420cc85fe1219c4770
jasonstevens13 <jasonstevens13@yahoo.com>
2020-05-09 23:00:14 -0400

    fixed routes parameters

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(express.json());
 
 // Routes:
 // notes.html route
-app.get("/", function (req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 // index.html route
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
-})
+});
 
 
 
