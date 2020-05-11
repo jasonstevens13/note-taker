5c6d1bd560354956146870a95a6f322135af8b4e
jasonstevens13 <jasonstevens13@yahoo.com>
2020-05-11 15:24:45 -0400

    created routes directory and brought the route code that's in progress over to the new routes.js file therein

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require("express");
+const bodyParser = require('body-parser');
 const path = require("path");
 const fs = require("fs");
 
@@ -13,57 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-let dbData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-
-// ROUTES
-
-// API Routes
-
-app.get("/api/notes", (req, res) => {
-    return res.json(dbData);
-});
+const routes = require('./routes/routes.js')(app, fs);
 
 
 
-// Create a post request to add new note
-app.post("/api/notes", (req, res) => {
-
-    let newNote = req.body;
-    // how do I add a new unique id here automatically?
-
-    // Do I have to read the db.json and then push the new note to it...??????
-    let savedNotes = dbData.push(json(newNote));
-
-    fs.writeFileSync("./db/db.json", savedNotes, (err, data) => {
-        if (err) throw err;
-
-        res.json(savedNotes);
-    });
-
-
-});
-
-
-
-
-
-
-
-// HTML Routes:
-// notes.html route
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-
-// // index.html route OR If no matching route is found default to this home
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
 
 
 
