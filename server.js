60ea64da1099817049791e4489f6c294143ec19d
jasonstevens13 <jasonstevens13@yahoo.com>
2020-05-10 22:41:42 -0400

    routes brought into server.js and routes directory folder deleted

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+// const OUTPUT_DIR = path.resolve(__dirname, "db");
+// const outputPath = path.join(OUTPUT_DIR, "db.json");
+
+// require("./public/assets/js/index");
+
+
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -12,14 +18,57 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// Routes:
+
+// ROUTES
+
+// HTML Routes:
 // notes.html route
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
 // index.html route
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// If no matching route is found default to home
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+
+// API Routes
+app.get("/api/notes", (req, res) => {
+    // Use db.json to store and retrieve notes using fs 
+    fs.readFile("./db/db.json", "utf-8", (err, dbData) => {
+        if (err) throw err;
+
+        const savedNotes = JSON.parse(dbData);
+
+        return savedNotes;
+    })
+
+});
+
+
+// Create a post request to add new note
+app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, dbData) => {
+        if (err) throw err;
+
+        const savedNotes = JSON.parse(dbData);
+        const newNote = req.body;
+        savedNotes.push(newNote);
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes), (err, data) => {
+            if (err) throw err;
+
+            res.json(newNote);
+        });
+    });
+
 });
 
 
@@ -29,3 +78,5 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
+
+
